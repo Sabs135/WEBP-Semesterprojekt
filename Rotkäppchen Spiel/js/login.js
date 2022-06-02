5a4bccf9f9926bc4380b5198c1e50110e4321abe
tkoelliker <tabea.koelliker@stud.fhgr.ch>
2022-06-02 22:39:35 +0200

    Fehlerbehebung id

--- Rotkäppchen Spiel/js/login.js
+++ Rotkäppchen Spiel/js/login.js
@@ -22,23 +22,15 @@ form.addEventListener('submit', function(e) {
     e.preventDefault()
     
     var name = document.getElementById('form_post_name').value
-    var id = Date.now()
-    localStorage.setItem('name', JSON.stringify(name));  /* Brauche ich für Anzeige auf spiel.html und highscore.html */
-    localStorage.setItem('spielerid', JSON.stringify(id)); //Spieler ID in local Sotrage speichern (für den Highscore, damit da die Spielerid mitgegeben werden kann)
-
-    //Anzeige in Konsole bzw. für RetrievedObject später
-    const userDetails = {
-        name: name = document.getElementById('form_post_name').value,
-        id: id = Date.now()
-    };
-
-    localStorage.setItem('userDetails', JSON.stringify(userDetails));  /* userDetails in Local Storage */
+    localStorage.setItem('name', JSON.stringify(name));  // Brauche ich für Anzeige auf spiel.html und highscore.html 
+    
         fetch('https://343505-26.web.fhgr.ch/api/jump-and-run/spieler/') //verbindung zur API spieler
         .then(response => {
             if (response.status === 500) { //wenn ein Fehler 500 erscheint
                 //Fehler
-                form.AddEventListener();
-                throw new Error("Hoppla. Etwas ist schiefgelaufen ... Error 500 - retry");
+                document.getElementById("show_user").innerHTML = 'Etwas ging nicht. Bitte logge dich noch einmal ein.'
+                throw new Error("Hoppla. Etwas ist schiefgelaufen ... Error 500");
+                
             }
             if (!response.ok) { //wenn generell ein Fehler erscheint, damit der User es mitbekommt
                 alert('geht nicht');
@@ -57,9 +49,27 @@ form.addEventListener('submit', function(e) {
                 show_user=document.getElementById("show_user")
                 text = '<p>Du konntest dich erfolgreich einloggen <b>' + result[index].name + '</b>!</p> <p> Deine Spieler-ID ist: <b>' + result[index].spielerid + '</b></p>'
                 show_user.innerHTML = text; //anzeige des Textes auf der Seite
+                localStorage.setItem('spielerid', JSON.stringify(result[index].spielerid)); //Spieler ID in local Sotrage speichern (für den Highscore, damit da die Spielerid mitgegeben werden kann)
+                
+                //Anzeige in Konsole bzw. für RetrievedObject später
+                const userDetails = {
+                    name: name = document.getElementById('form_post_name').value,
+                    id: id = result[index].spielerid
+                };
+                localStorage.setItem('userDetails', JSON.stringify(userDetails));  /* userDetails in Local Storage */
             } 
 
             if (result.find(findName) === undefined) { //schaut ob eigetragener Name in der API nicht zu finden ist --> wenn er nicht zu finden ist, wird er registriert
+                var id = Date.now()
+                localStorage.setItem('spielerid', JSON.stringify(id)); //Spieler ID in local Sotrage speichern (für den Highscore, damit da die Spielerid mitgegeben werden kann)
+
+                //Anzeige in Konsole bzw. für RetrievedObject später
+                const userDetails = {
+                    name: name = document.getElementById('form_post_name').value,
+                    id: id = Date.now()
+                };
+                localStorage.setItem('userDetails', JSON.stringify(userDetails));  /* userDetails in Local Storage */
+
                 fetch('https://343505-26.web.fhgr.ch/api/jump-and-run/spieler/', {
                     method: 'POST', 
                     body: JSON.stringify({
@@ -74,8 +84,8 @@ form.addEventListener('submit', function(e) {
                 .then(response => {
                     if (response.status === 500) { //wenn ein Fehler 500 erscheint
                         //Fehler
-                        form.AddEventListener();
-                        throw new Error("Hoppla. Etwas ist schiefgelaufen ... Error 500 - retry");
+                        document.getElementById("show_user").innerHTML = 'Etwas ging nicht. Bitte registriere dich noch einmal ein.'
+                        throw new Error("Hoppla. Etwas ist schiefgelaufen ... Error 500");
                     }
                     if (!response.ok) { //wenn generell ein Fehler erscheint, damit der User es mitbekommt
                         alert('geht nicht');
@@ -92,6 +102,9 @@ form.addEventListener('submit', function(e) {
                 .catch(error => console.error('Error:', error)); //Error abfangen
             }
         });
+
+        
+        
 });
 
 //-------- ENDE USER hinzufügen ---------/
